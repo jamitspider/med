d16e70ee32ccece6168dacb56b683d0b352348ec
jamit <jamit@spiderorb.com>
2018-04-20 18:37:08 +0530

    added driver in local folder

--- environment.py
+++ environment.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 def before_all(context):
 #     context.driver = webdriver.Firefox(executable_path='/Users/jamit/Downloads/driver/geckodriver')
-    context.driver = webdriver.Chrome(executable_path='/Users/jamit/Downloads/driver/chromedriver')
+    context.driver = webdriver.Chrome(executable_path='./files/driver/chromedriver')
     context.driver.maximize_window()
     context.driver.implicitly_wait(30)
 def after_all(context):
