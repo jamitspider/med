49d9da38e8ee2b7826b946c37fe20a24a79fa83d
jamit <jamit@spiderorb.com>
2018-04-23 14:57:55 +0530

    hide max window

--- environment.py
+++ environment.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 def before_all(context):
 #     context.driver = webdriver.Firefox(executable_path='/Users/jamit/Downloads/driver/geckodriver')
     context.driver = webdriver.Chrome(executable_path='./files/driver/chromedriver')
-    context.driver.maximize_window()
+#     context.driver.maximize_window()
     context.driver.implicitly_wait(30)
 def after_all(context):
     context.driver.quit()
